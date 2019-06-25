490c97cb72d8d3196600e45a34aac7e21decb6a3
Bryant <cabrera.bryant@gmail.com>
2019-06-24 20:19:47 -0700

    Bryant refactors tests to avoid test pollution.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,36 +4,53 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+// gather related tests and group them together in a describe function
+// refactoring into describe will make the old snapshot obsolete, so press u in test command terminal to update snapshot
+describe('App', () => {
+    const app = shallow(<App />);
 
-// use a global it function
+    // use a global it function
     // parameters: ("description of test", function for test)
-it('renders correctly', () => { 
-    expect(app).toMatchSnapshot();
-});
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
 
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    // find Add give button
-        // finds class name
-        // use simulations they provide
-    app.find('.btn-add').simulate('click');
+    // can create nested describe buttons for tests that simulate the same action
+    describe('when clicking the `add-gift` button', () => {
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
 
-    // make sure state updates
-        // to create unique IDs, we get their index +1 so we always get a unique ID for the next object
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+        afterEach(() => {
+            app.setState({
+                gifts: []
+            });
+        });
 
-// BEWARE: test pollution
-    // a.	= when one test affects the others
+        it('adds a new gift to `state`', () => {
+            // find Add give button
+            // finds class name
+            // use simulations they provide
+            // app.find('.btn-add').simulate('click');
+
+            // make sure state updates
+            // to create unique IDs, we get their index +1 so we always get a unique ID for the next object
+            expect(app.state().gifts).toEqual([{ id: 1 }]);
+        });
+
+        // BEWARE: test pollution
+        // a.	= when one test affects the others
         // i.E.g.updating state
-    // b.Ideally, we want each test to be an isolated case
-it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
+        // b.Ideally, we want each test to be an isolated case
+        it('adds a new gift to the rendered list', () => {
+            // app.find('.btn-add').simulate('click');
 
-    // .children() returns an array of the inner child nodes of the node that we're looking at.  can look at its length
-    expect(app.find('.gift-list').children().length).toEqual(2);
+            // .children() returns an array of the inner child nodes of the node that we're looking at.  can look at its length
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    }); 
 });
