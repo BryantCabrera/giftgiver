5a3646fe943f8d06efd6ca1a80b84227042d5c18
Bryant <cabrera.bryant@gmail.com>
2019-06-24 20:44:01 -0700

    Bryant creates tests to simulate input change and updating state in Gift.js based on that change.

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -15,4 +15,20 @@ describe('Gift', () => {
             present: ''
         });
     });
+
+    describe('when typing into the person input', () => {
+        const person = 'Uncle';
+
+        beforeEach(() => {
+            // change event = simulates a user typing into an input and changing it
+                // we have to specify what we want the input to change to
+            gift.find('.input-person').simulate('change', {
+                target: { value: person }
+            });
+        });
+
+        it('updates the person in `state`', () => {
+            expect(gift.state().person).toEqual(person);
+        })
+    })
 });
